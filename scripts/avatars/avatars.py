552737e8a82031ce51dbfb56025f8ac5d49e7d87
Niels Lohmann <mail@nlohmann.me>
2022-08-04 14:29:11 +0200

    :wrench: detect contributors that are not listed in README file

--- scripts/avatars/avatars.py
+++ scripts/avatars/avatars.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 
+from typing import List, Optional
 import requests
 import re
 import os.path
@@ -12,7 +13,7 @@ CONFIG = json.load(open(os.path.join(os.path.dirname(__file__), '..', '..', 'con
 GITHUB_AUTH=(CONFIG['GITHUB_AUTH']['user'], CONFIG['GITHUB_AUTH']['key'])
 
 
-def get_images_from_readme(path):
+def get_images_from_readme(path: Optional[str]) -> List[str]:
     github_user_re = re.compile(r'https://github.com/([a-zA-Z0-9-_\-]+)')
 
     if path is None:
@@ -41,12 +42,14 @@ def get_images_from_readme(path):
         except KeyError:
             pass
 
+    return list(sorted(usernames))
+
 
 def create_large_image():
     size = 64
 
     image_files = glob.glob('cache/*')
-    result = Image.new("RGB", (3 * size, 83 * size))
+    result = Image.new("RGB", (4 * size, 79 * size))
 
     x_index = 0
     y_index = 0
@@ -67,28 +70,57 @@ def create_large_image():
     result.save(os.path.expanduser('avatars.png'))
 
 
-def get_contributors_images():
-    url = 'https://api.github.com/repos/nlohmann/json/contributors?page={page}'
-    pages = 3
+def get_contributors_images() -> List[str]:
+    url = 'https://api.github.com/repos/nlohmann/json/contributors'
 
-    for page in range(1, pages+1):
-        contributors = requests.get(url.format(page=page), auth=GITHUB_AUTH).json()
+    page = 1
+    usernames = []
+    while True:
+        res = requests.get(url=url, params={'page': page, 'per_page': 100}, auth=GITHUB_AUTH)
+        if not res.ok:
+            break
+
+        contributors = res.json()
+
+        if len(contributors) == 0:
+            break
+
+        page += 1
+
         for contributor in contributors:
-            print(contributor['login'])
+            username = contributor['login']
+            print(username)
+            usernames.append(username)
 
-            if os.path.exists('cache/{username}'.format(username=contributor['login'])):
+            if os.path.exists(f'cache/{username}'):
                 continue
 
             try:
                 image = requests.get(contributor['avatar_url']).content
                 if len(image):
-                    with open('cache/{username}'.format(username=contributor['login']), 'wb') as code:
+                    with open(f'cache/{username}', 'wb') as code:
                         code.write(image)
             except KeyError:
                 pass
 
+    return sorted(usernames)
+
 
 if __name__ == '__main__':
-    get_images_from_readme(path=sys.argv[1] if len(sys.argv) > 1 else None)
-    get_contributors_images()
+    usernames_readme = get_images_from_readme(path=sys.argv[1] if len(sys.argv) > 1 else None)
+    usernames_all = get_contributors_images()
+
+    # these accounts have not contributed via a merged PR (or are bots)
+    non_contributors = ['cjh1', 'remyabel2', 'Francois-air', 'mstan-xx',
+                        'dmenendez-gruposantander', 'palmer-dabbelt',
+                        'intelmatt', 'mend-bolt-for-github[bot]', 'bradfora']
+
+    found_missing_contributor = False
+    for user in (set(usernames_all) - set(non_contributors)) - set(usernames_readme):
+        print(f'⚠️ {user} is not in README')
+        found_missing_contributor = True
+
+    if found_missing_contributor:
+        sys.exit(1)
+
     create_large_image()
