baacda12401baed44a06a5b89eca2dbcc233370f
Niels Lohmann <mail@nlohmann.me>
2022-08-07 22:43:12 +0200

    :wrench: add code to optionally add more braches

--- scripts/clone/build.sh
+++ scripts/clone/build.sh
@@ -3,4 +3,8 @@ set -euxo pipefail
 
 test -d workdir/json || scripts/clone/git-flow-clone https://github.com/nlohmann/json.git workdir/json
 
-cd workdir/json ; git config gitflow.prefix.versiontag v
+cd workdir/json
+git config gitflow.prefix.versiontag v
+
+# add some more branches
+# GIT_MERGE_AUTOEDIT=no git merge develop origin/release_json_fwd
